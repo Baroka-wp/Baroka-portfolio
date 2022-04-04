a3d98d86ebcdbf24b433c14c8be390e9d9b33e20
baroka <birotori@gmail.com>
2022-04-04 21:05:34 +0100

    Fix linter reports

--- js/script.js
+++ js/script.js
@@ -1,38 +1,35 @@
 const toolbar = document.querySelector('.toolbar');
 const subContainer = document.querySelector('.sub-container');
-const navDeskVersion = document.querySelector('.nav-bar-desk-version');
 const sections = document.querySelectorAll('section');
 const navA = document.querySelectorAll('.nav-bar-desk-version ul a');
 
+toolbar.addEventListener('click', () => {
+  toolbar.classList.toggle('active');
+  subContainer.classList.toggle('active');
+});
 
-toolbar.addEventListener("click", () => {
-  toolbar.classList.toggle("active");
-  subContainer.classList.toggle("active");
-})
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  toolbar.classList.remove('active');
+  subContainer.classList.remove('active');
+}));
 
-document.querySelectorAll(".nav-link").forEach(n =>
-  n.addEventListener("click", () => {
-      toolbar.classList.remove("active");
-      subContainer.classList.remove("active");
-  })
-);
-
-window.addEventListener("scroll", () => {
-  let currentSection = "";
-  sections.forEach(section => {
+window.addEventListener('scroll', () => {
+  let currentSection = '';
+  sections.forEach((section) => {
     const sectionTop = section.offsetTop;
-    const sectionHeight = section.clientHeight;
-    if(pageYOffset < 800){
-      currentSection = "hello";
-    }else if(pageYOffset >= sectionTop) {
+    // eslint-disable-next-line no-restricted-globals
+    if (pageYOffset < 800) {
+      currentSection = 'hello';
+    // eslint-disable-next-line no-restricted-globals
+    } else if (pageYOffset >= sectionTop) {
       currentSection = section.getAttribute('id');
-    };
+    }
   });
-  navA.forEach(a => {
+  navA.forEach((a) => {
     a.classList.remove('active');
-    if(a.classList.contains(currentSection)) {
-      document.querySelector('.hello').classList.remove('active')
-      a.classList.add('active')
-    };
-  })
+    if (a.classList.contains(currentSection)) {
+      document.querySelector('.hello').classList.remove('active');
+      a.classList.add('active');
+    }
+  });
 });
