428999f98ed992b8af56d933671816e71ae475dd
baroka <birotori@gmail.com>
2022-04-04 20:48:33 +0100

    Add highlight the name of the section in the menu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-const toolbar = document.querySelector('.toolbar')
-const subContainer = document.querySelector('.sub-container')
+const toolbar = document.querySelector('.toolbar');
+const subContainer = document.querySelector('.sub-container');
+const navDeskVersion = document.querySelector('.nav-bar-desk-version');
+const sections = document.querySelectorAll('section');
+const navA = document.querySelectorAll('.nav-bar-desk-version ul a');
+
 
 toolbar.addEventListener("click", () => {
   toolbar.classList.toggle("active");
@@ -10,4 +14,25 @@ document.querySelectorAll(".nav-link").forEach(n =>
   n.addEventListener("click", () => {
       toolbar.classList.remove("active");
       subContainer.classList.remove("active");
-  }));
+  })
+);
+
+window.addEventListener("scroll", () => {
+  let currentSection = "";
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if(pageYOffset < 800){
+      currentSection = "hello";
+    }else if(pageYOffset >= sectionTop) {
+      currentSection = section.getAttribute('id');
+    };
+  });
+  navA.forEach(a => {
+    a.classList.remove('active');
+    if(a.classList.contains(currentSection)) {
+      document.querySelector('.hello').classList.remove('active')
+      a.classList.add('active')
+    };
+  })
+});
